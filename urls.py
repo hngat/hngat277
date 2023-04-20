488178fbef4a982bef33ba28b02f17e8f84a6608
hngat <120127475+hngat@users.noreply.github.com>
2023-04-20 20:25:29 +0700

    item

--- urls.py
+++ urls.py
@@ -1,19 +1,15 @@
 
-from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
-from .forms import LoginForm
 
-app_name = 'core'
+app_name = 'item'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('contact/', views.contact, name='contact'),
-    path('introduce/', views.introduce, name='introduce'),
-    path('csbaomat/', views.csbaomat, name='csbaomat'),
-    path('dieukhoansd/', views.dieukhoansd, name='dieukhoansd'),
-    
-    path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
+    path('', views.items, name='items'),
+    path('new/', views.new, name='new'),
+
+    path('<int:pk>/', views.detail, name='detail'),
+    path('<int:pk>/delete/', views.delete, name='delete'),
+    path('<int:pk>/edit/', views.edit, name='edit'),
 ]
--- views.py
+++ views.py
@@ -1,38 +1,79 @@
-from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404, redirect
 
-from item.models import Category, Item
+from .forms import NewItemForm, EditItemForm
+from .models import Category, Item
 
-from .forms import SignupForm
-
-def index(request):
-    items = Item.objects.filter(is_sold=False)[0:6]
+def items(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
     categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
 
-    return render(request, 'core/index.html', {
-        'categories': categories,
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'item/items.html', {
         'items': items,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id)
     })
 
-def contact(request):
-    return render(request, 'core/contact.html')
-def introduce(request):
-    return render(request, 'core/introduce.html')
-def csbaomat(request):
-    return render(request, 'core/csbaomat.html')
-def dieukhoansd(request):
-    return render(request, 'core/dieukhoansd.html')
+def detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
 
-def signup(request):
+    return render(request, 'item/detail.html', {
+        'item': item,
+        'related_items': related_items
+    })
+
+@login_required
+def new(request):
     if request.method == 'POST':
-        form = SignupForm(request.POST)
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = NewItemForm()
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'New item',
+    })
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
 
         if form.is_valid():
             form.save()
 
-            return redirect('/login/')
+            return redirect('item:detail', pk=item.id)
     else:
-        form = SignupForm()
+        form = EditItemForm(instance=item)
 
-    return render(request, 'core/signup.html', {
-        'form': form
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit item',
     })
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+
+    return redirect('dashboard:index')
